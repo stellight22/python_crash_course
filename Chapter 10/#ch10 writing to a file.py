fcd6693871afbe994ea3ce62f87504f4dcd86799
Stella <58281413+stellight22@users.noreply.github.com>
2021-09-15 19:00:54 -0700

    test test
    checking that the commits are properly updated

--- Chapter 10/#ch10 writing to a file.py
+++ Chapter 10/#ch10 writing to a file.py
@@ -5,6 +5,7 @@ filename = 'learning_python.txt'
 with open(filename, 'w') as file_object:
     file_object.write("bleh bleh bleh\n")
     file_object.write("this is the second line of bleh\n")
+    file_object.write("This is the third bleh bleh bleh\n")
 
 #appending
 with open(filename, 'a') as file_object:
