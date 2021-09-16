9126f39cad6cab71cbc2e17dcd32f519e37dd00a
Stella <58281413+stellight22@users.noreply.github.com>
2021-09-16 08:30:00 -0700

    Ch 11 update

---
+++ Ch11/employee.py
@@ -0,0 +1,10 @@
+#Ch 11-3
+class Employee():
+    def __init__(self, f_name, l_name, salary):
+        self.f_name = f_name
+        self.l_name = l_name
+        self.salary = salary
+
+    def give_raise(self, raise = 5000):
+        self.salary += raise
+    
