5cd84795a22906a558375d3fb9c5e25982418df6
Stella <stella.codes1022@gmail.com>
2021-10-08 15:02:31 -1000

    functions completion

---
+++ Revision/dice.py
@@ -0,0 +1,16 @@
+from random import randint
+
+class Die():
+    def __init__(self, sides = 6):
+        self.sides = sides
+
+    def roll_die(self):
+        r = randint(1, self.sides)
+        print(f"your roll:{r}, sides: {self.sides}")
+
+d = Die()
+
+x = 1
+while x < 11:
+    d.roll_die()
+    x += 1
---
+++ Revision/dice_forloop.py
@@ -0,0 +1,15 @@
+from random import randint
+
+class Die():
+    def __init__(self, sides = 6):
+        self.sides = sides
+
+    def roll_die(self):
+        r = randint(1, self.sides)
+        print(f"your roll:{r}, sides: {self.sides}")
+
+d = Die()
+
+for x in range(1,11):
+    d.roll_die()
+
